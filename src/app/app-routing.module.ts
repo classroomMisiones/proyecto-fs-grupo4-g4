cc140e232cf81631fc4fdeaac8d2afa777b4839d
guillermovillalba <brahianvillalba23@gmail.com>
2021-07-14 15:02:16 -0300

    LuckyPay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AyudaComponent } from './inicio/ayuda/ayuda.component';
+import { PrincipalComponent } from './inicio/principal/principal.component';
+import { QuienessomosComponent } from './inicio/quienessomos/quienessomos.component';
+import { RegistracionComponent } from './inicio/registracion/registracion.component';
+import { TransferenciaComponent } from './inicio/transferencia/transferencia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:"quienessomos",
+ component:QuienessomosComponent},
+ {path:"registracion",
+ component:RegistracionComponent
+ },
+ {
+   path:"inicio",
+   component:AppComponent
+ },
+ {
+   path:"trasnferencia",
+   component:TransferenciaComponent
+ },
+ {
+   path:"iniciarsesion", 
+   component:PrincipalComponent
+ },
+ {
+   path:"ayuda",
+   component:AyudaComponent 
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './inicio/navbar/navbar.component';
+import { FooterComponent } from './inicio/footer/footer.component';
+import { QuienessomosComponent } from './inicio/quienessomos/quienessomos.component';
+import { RegistracionComponent } from './inicio/registracion/registracion.component';
+import { TransferenciaComponent } from './inicio/transferencia/transferencia.component';
+import { PrincipalComponent } from './inicio/principal/principal.component';
+import { AyudaComponent } from './inicio/ayuda/ayuda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    QuienessomosComponent,
+    RegistracionComponent,
+    TransferenciaComponent,
+    PrincipalComponent,
+    AyudaComponent,
+    
   ],
   imports: [
     BrowserModule,
