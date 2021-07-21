d6451928f980815003d6e8ed6a75847f9ba60520
lucasmonges <86086104+lucasmonges@users.noreply.github.com>
2021-07-20 23:15:45 -0300

    Update 20/07/2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AyudaComponent } from './inicio/ayuda/ayuda.component';
-import { PrincipalComponent } from './inicio/principal/principal.component';
-import { QuienessomosComponent } from './inicio/quienessomos/quienessomos.component';
-import { RegistracionComponent } from './inicio/registracion/registracion.component';
-import { TransferenciaComponent } from './inicio/transferencia/transferencia.component';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
- { path:"quienessomos",
- component:QuienessomosComponent},
- {path:"registracion",
- component:RegistracionComponent
- },
- {
-   path:"inicio",
-   component:AppComponent
- },
- {
-   path:"trasnferencia",
-   component:TransferenciaComponent
- },
- {
-   path:"iniciarsesion", 
-   component:PrincipalComponent
- },
- {
-   path:"ayuda",
-   component:AyudaComponent 
- }
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './inicio/navbar/navbar.component';
-import { FooterComponent } from './inicio/footer/footer.component';
-import { QuienessomosComponent } from './inicio/quienessomos/quienessomos.component';
-import { RegistracionComponent } from './inicio/registracion/registracion.component';
-import { TransferenciaComponent } from './inicio/transferencia/transferencia.component';
-import { PrincipalComponent } from './inicio/principal/principal.component';
-import { AyudaComponent } from './inicio/ayuda/ayuda.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     NavbarComponent,
     FooterComponent,
-    QuienessomosComponent,
-    RegistracionComponent,
-    TransferenciaComponent,
-    PrincipalComponent,
-    AyudaComponent,
-    
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -31,3 +24,4 @@ import { AyudaComponent } from './inicio/ayuda/ayuda.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
