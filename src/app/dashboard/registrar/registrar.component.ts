5e77dfe30a5913edc2669ea6398f34a4e3bcd051
lucasmonges <86086104+lucasmonges@users.noreply.github.com>
2021-09-12 21:13:45 -0300

    Update con validaciones en Angular

--- src/app/dashboard/registrar/registrar.component.ts
+++ src/app/dashboard/registrar/registrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.myForm = this.fb.group({
+      apellido: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      documento: ['',[Validators.required, Validators.maxLength(8)]],
+      correo: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.myForm.value);
   }
 
 }
