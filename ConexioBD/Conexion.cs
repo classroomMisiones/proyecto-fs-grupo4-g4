80a20dce41768b49c64908bbe9b52ecd460e52b8
Roberto <roberto.sueldo@gmail.com>
2021-09-16 17:01:09 -0300

    Se agreo la carpeta con la Clase conexion

---
+++ ConexioBD/Conexion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace yaguarete.Controllers
+{
+    public class Conexion
+    {
+        private static string conexion = ConfigurationManager.ConnectionStrings["yaguarete"].ConnectionString;
+        private static SqlConnection conector = new SqlConnection(conexion);
+
+        public static DataTable select(string tabla)
+        {
+            DataTable retorno = new DataTable();
+            conector.Open();
+            SqlDataAdapter adaptador = new SqlDataAdapter("Select * from "+tabla, conector);
+            adaptador.Fill(retorno);
+            conector.Close();
+            return retorno;
+        }
+
+        public static DataTable select(string tabla,string condicion)
+        {
+            DataTable retorno = new DataTable();
+            conector.Open();
+            SqlDataAdapter adaptador = new SqlDataAdapter("Select * from " + tabla + " where " + condicion, conector );
+            adaptador.Fill(retorno);
+            conector.Close();
+            return retorno;
+            
+        }
+
+        public static void insert(string sql) 
+        {
+
+            /* SqlCommand cmd = new SqlCommand(sql, conector);
+             conector.Open();
+             cmd.ExecuteNonQuery();
+
+             conector.Close();*/
+           
+            conector.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = conector;
+           
+                cmd.ExecuteNonQuery();
+          
+
+          
+            conector.Close();
+
+        }
+    }
+}
+
---
+++ ConexioBD/ProvinciaController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace yaguarete.Controllers
+{
+    public class ProvinciaController : ApiController
+    {
+
+        // GET: api/Provincia
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            //return new string[] { "value1", "value2" };
+            DataTable prov = new DataTable();
+            /* using (SqlConnection conector = new SqlConnection(conexion))
+             {
+                 conector.Open();
+                 SqlDataAdapter adaptador = new SqlDataAdapter("Select * from provincias",conector);
+                 adaptador.Fill(prov);
+             }*/
+            prov = Conexion.select("Provincias");
+            return Ok(prov);
+        }
+
+
+        // GET: api/Provincia/5
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            DataTable prov = new DataTable();
+            prov = Conexion.select("Provincias", "Id_Provincia = " + id);
+            return Ok(prov);
+        }
+
+        /* // POST: api/Provincia
+         public void Post([FromBody] string value)
+         {
+         }*/
+
+        // POST: api/Provincia
+
+        [HttpPost]
+        public IHttpActionResult Post(Models.Provincia unaProvincia)
+        {
+            string msg = "Todo OK!!!!!!!!";
+           // try
+          //  {
+                string sql = "insert into Provincias (provincia) values ('" + unaProvincia.NomProvincia + "')";
+                Conexion.insert(sql);
+           // }catch(SqlException ex)
+          //  {
+              //  msg = ex.Message;
+               
+           // }
+            return Ok(msg);
+    
+        }
+
+        // PUT: api/Provincia/5
+        public void Put(int id, [FromBody] string value)
+        {
+           
+        }
+
+        // DELETE: api/Provincia/5
+        public void Delete(int id)
+        {
+        }
+
+     
+    }
+}
